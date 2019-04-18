65395524770406b1937d386adac4b0fcfef34d11
waynewu6250 <waynewu6250@gmail.com>
2019-04-18 14:39:54 +0800

    Update

--- api.py
+++ api.py
@@ -6,21 +6,22 @@ import random
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-tasks = [
-     {'id': 1, 'title': "Test Task 1", 'is_completed': 'true'},
-     {'id': 2, 'title': "Test Task 2", 'is_completed': 'false'},
-     {'id': 3, 'title': "Test Task 3", 'is_completed': 'false'}
-]
+def query_handle(query):
+    db = MySQLdb.connect("mysql-server", "root", "secret", "testdb")
+    cursor = db.cursor()
+    cursor.execute(query)
+    return db, cursor
+
 
 # create new task and put it in mysql DB
 @app.route('/', methods=["POST"])
 def create_task():
-
-    # check if new id exist in current ids
-    current_id = []
-    for task in tasks:
-        current_id.append(task['id'])
+    
+    query =  "SELECT * FROM tasks"
+    db, cursor = query_handle(query)
+    results = cursor.fetchall()
+    current_id = [row[0] for row in results]
+    db.close()
 
     new_id = 1
     while new_id in current_id:
@@ -29,12 +30,9 @@ def create_task():
     new_task = {'id': new_id}
     new_task['title'] = request.json['title']+" "+str(new_id)
     new_task['is_completed'] = request.json['is_completed']
-    tasks.append(new_task)
 
     query = "INSERT INTO tasks(id, title, is_completed) VALUES (%s, '%s', %s)" % (new_task['id'], new_task['title'], new_task['is_completed'])
-    db = MySQLdb.connect("mysql-server", "root", "secret", "testdb")
-    cursor = db.cursor()
-    cursor.execute(query)
+    db, cursor = query_handle(query)
     db.commit()
     db.close()
 
@@ -44,7 +42,18 @@ def create_task():
 # list all task
 @app.route('/', methods=['GET'])
 def list_all():
-    # convert lists and dictionaries to JSON format
+
+    tasks = []
+    query =  "SELECT * FROM tasks"
+    db, cursor = query_handle(query)
+    results = cursor.fetchall()
+    for row in results:
+        task = {'id': row[0]}
+        task['title'] = row[1]
+        task['is_completed'] = row[2]
+        tasks.append(task)
+    db.close()
+
     return jsonify(tasks), 200
 
 
@@ -53,56 +62,74 @@ def list_all():
 @app.route('/<id>', methods=["GET"])
 def get_task(id):
     response = []
-    for task in tasks:
-        if task['id'] == int(id):
-            response.append(task)
+    query =  "SELECT * FROM tasks"
+    db, cursor = query_handle(query)
+    results = cursor.fetchall()
+    for row in results:
+        if row[0] == int(id):
+            response.append({'id': row[0], 'title': row[1], 'is_completed': row[2]})
+            break
+    db.close()
         
     if response:
         return jsonify(response), 200
     else:
         return jsonify("error: There is no task at that id"), 404
-
-# delete a specific task
-@app.route('/<id>', methods=["DELETE"])
-def delete_task(id):
-    for task in tasks:
-        if task['id'] == int(id):
-            tasks.remove(task)
-    return 204
-
     
 # edit the task
 @app.route('/<id>', methods=["PUT"])
 def edit_task(id):
 
-    current_id = []
-    for task in tasks:
-        current_id.append(task['id'])
+    new_task = {'id': id}
+    new_task['title'] = request.json['title']+" "+str(id)
+    new_task['is_completed'] = request.json['is_completed']
     
-    if int(id) not in current_id:
+    flag = False
+    query =  "SELECT * FROM tasks"
+    db, cursor = query_handle(query)
+    results = cursor.fetchall()
+    for row in results:
+        if row[0] == int(id):
+            query = "UPDATE tasks SET title = '%s', is_completed = %s WHERE id = %s" % (new_task['title'], new_task['is_completed'], new_task['id'])
+            cursor.execute(query)
+            db.commit()
+            flag = True
+            break
+    db.close()
+    
+    if not flag:
         return jsonify("error: There is no task at that id"), 404
-
     else:
-        for task in tasks:
-            if task['id'] == int(id):
-                task['title'] = request.json['title']
-                task['is_completed'] = request.json['is_completed']
-        return 204
+        return jsonify(new_task), 204
 
+# delete a specific task
+@app.route('/<id>', methods=["DELETE"])
+def delete_task(id):
+    flag = False
+    query =  "SELECT * FROM tasks"
+    db, cursor = query_handle(query)
+    results = cursor.fetchall()
+    for row in results:
+        if row[0] == int(id):
+            cursor.execute("DELETE FROM tasks WHERE id = %s" % id)
+            db.commit()
+            flag = True
+            break
+    db.close()
+    
+    if not flag:
+        return jsonify("error: There is no task at that id"), 404
+    else:
+        return '204'
 
 @app.route('/', methods=["DELETE"])
 def bulk_delete_task():
-    delete_list = request.json['tasks']
-    delete_id = []
-    for ids in delete_list:
-        delete_id.append(ids['id'])
-    
-    for did in delete_id:
-        for task in tasks:
-            if task['id'] == did:
-                tasks.remove(task)
+    query =  "DELETE FROM tasks"
+    db, _ = query_handle(query)
+    db.commit()
+    db.close()
 
-    return 204
+    return '204'
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
